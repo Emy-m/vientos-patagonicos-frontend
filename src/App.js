081965b830c7843c639be181801360b7697d6fde
Emilio Martin <emiunrn@gmail.com>
2022-08-23 22:09:10 -0300

    add clients table component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import AddClientForm from "./src/components/Clients/AddClientForm";
+import ShowClientsTable from "./src/components/Clients/ShowClientsTable";
 
 function App() {
   return (
     <div className="App">
       <AddClientForm />
+      <ShowClientsTable />
     </div>
   );
 }
--- src/src/components/Clients/ShowClientsTable.js
+++ src/src/components/Clients/ShowClientsTable.js
@@ -1,7 +1,74 @@
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+  Button,
+} from "@mui/material";
 
 function ShowClientsTable() {
-  return <div>ShowClientsTable</div>;
+  const [clients, setClients] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    fetchClients();
+  }, []);
+
+  const fetchClients = () => {
+    setLoading(true);
+    setError(null);
+
+    fetch("http://localhost:7070/clientes")
+      .then((response) => response.json())
+      .then((data) => {
+        setClients(data.clientes);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <Typography component="h2" variant="h6" color="primary" gutterBottom>
+        Clients
+      </Typography>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Firstname</TableCell>
+            <TableCell>Lastname</TableCell>
+            <TableCell>DNI</TableCell>
+            <TableCell>Email</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {clients.map((client) => (
+            <TableRow key={client.id}>
+              <TableCell>{client.nombre}</TableCell>
+              <TableCell>{client.apellido}</TableCell>
+              <TableCell>{client.dni}</TableCell>
+              <TableCell>{client.email}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => fetchClients()}
+        disabled={loading}
+      >
+        Refresh
+      </Button>
+    </React.Fragment>
+  );
 }
 
 export default ShowClientsTable;
